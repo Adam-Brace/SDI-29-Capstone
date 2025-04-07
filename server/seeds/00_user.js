b3c0c54a6edc6d20528b80061893c565683bd847
ABazing <jessbaze@gmail.com>
2025-04-07 18:33:27 +0200

    - seeds completed

--- server/seeds/00_user.js
+++ server/seeds/00_user.js
@@ -1,13 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+exports.seed = async function (knex) {
+  await knex('users').del()
+  await knex('users').insert([
+    { first_name: "John", last_name: "Jacob", rank: "Sgt", email: "john.jacob1@example.com", phone: "999-999-9999", organization: "Delta 1", crew: "Bravo", position: "SVO", permissions: "user" },
+    { first_name: "Patrick", last_name: "Johnson", rank: "Cpt", email: "patric.johnson@example.com", phone: "999-999-9999", organization: "Delta 1", crew: "Front Office", position: "CSS", permissions: "admin" },
+    { first_name: "Frank", last_name: "Smith", rank: "SMSgt", email: "frank.smith@example.com", phone: "999-999-9999", organization: "Delta 1", crew: "Alpha", position: "CCH", permissions: "user" },
+    { first_name: "Sarah", last_name: "Young", rank: "TSgt", email: "sarah.young@example.com", phone: "999-999-9999", organization: "Delta 1", crew: "Charlie", position: "SSO", permissions: "user" }
   ]);
 };
