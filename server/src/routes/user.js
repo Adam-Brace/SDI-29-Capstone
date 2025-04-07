944168f2e2682ae77e319bece9305b5869dcaaba
Adam-Brace <adambrace03@gmail.com>
2025-04-07 14:07:18 -0400

    user routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -108,4 +108,33 @@ router.patch("/:id", async (req, res) => {
 		.catch((err) => res.status(500).json({ error: err.message }));
 });
 
+router.delete("/:id", (req, res) => {
+	knex("user")
+		.where("id", req.params.id)
+		.del()
+		.then(() => res.status(200).json({ message: "User deleted" }))
+		.catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.post("/login", async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		const user = await knex("user").where({ email }).first();
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const isMatch = await argon2.verify(user.password, password);
+		if (!isMatch) {
+			return res.status(401).json({ error: "Incorrect password" });
+		}
+
+		res.json({ message: "Login successful", user });
+	} catch (error) {
+		console.error("Error logging in:", error);
+		res.status(500).json({ error: "Failed to login" });
+	}
+});
+
 module.exports = router;
