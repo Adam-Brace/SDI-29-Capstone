c5e2ed2e79cb1ae7ae4943b60cfb53ea20c03cba
jessicaghunt <huntjessicag@gmail.com>
2025-04-07 11:59:24 -0400

    Migrations complete-ish

--- server/migrations/01_20250407151620_events.js
+++ server/migrations/01_20250407151620_events.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("events", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id");
+    table.foreign("user_id").references("user.id");
+    // Dummy data needed
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .afterTable("events", (table) => {
+      table.dropForeign("user_id");
+    })
+    .then(function () {
+      return knex.schema.dropTableIfExists("events");
+    });
 };
