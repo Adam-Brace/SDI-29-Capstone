ac1959e814da285c00a21c24a2aae530b20606b7
jessicaghunt <huntjessicag@gmail.com>
2025-04-08 12:23:41 -0400

    two tests passed in App.test.jsx

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,9 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import { MemoryRouter } from "react-router-dom";
 
-test("renders Hello World", () => {
-	render(<App />);
+describe("App component", () => {
+  test("renders Hello World", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
 
-	const linkElement = screen.getByText(/Hello World/i);
-	expect(linkElement).toBeInTheDocument();
+    const linkElement = screen.getByText(/Hello World/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  //Test for Theme selection
+  test("render theme toggle", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const toggleThemeButton = screen.getByRole("button", {
+      name: /Switch to Dark Mode/,
+    });
+    expect(document.body.className).toBe("light-theme");
+
+    fireEvent.click(toggleThemeButton);
+    expect(document.body.className).toBe("dark-theme");
+
+    fireEvent.click(toggleThemeButton);
+    expect(document.body.className).toBe("light-theme");
+  });
 });
