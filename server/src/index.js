ef8f11275bf347e8fc905af998906e8b38ac5cdc
Adam-Brace <adambrace03@gmail.com>
2025-04-09 10:54:11 -0400

    initial setup

--- server/src/index.js
+++ server/src/index.js
@@ -11,17 +11,40 @@ if (!PORT) {
 	PORT = process.env.SERVER_PORT;
 }
 
+const socket = http.createServer(app);
+const io = new Server(socket, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
+
 app.use(cors());
 app.use(express.json());
 
 const userRoute = require("./routes/user");
 const eventsRoute = require("./routes/events");
+const messageRoute = require("./routes/message");
 
 app.use("/user", userRoute);
 app.use("/events", eventsRoute);
+app.use("/message", messageRoute);
 
-const server = app.listen(PORT, () => {
-	console.log(`App listening at http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+	console.log("User connected:", socket.id);
+
+	socket.on("send_message", (data) => {
+		// optionally save to DB here
+		io.emit("receive_message", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected", socket.id);
+	});
+});
+
+const server = socket.listen(PORT, () => {
+	console.log(`Server listening at http://localhost:${PORT}`);
 });
 
 module.exports = { app, server, PORT };
