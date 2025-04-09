a5d53d74b723a05b82edc5d927bc0b42e3d68cc5
Adam-Brace <adambrace03@gmail.com>
2025-04-09 14:49:42 -0400

    migrating seeding and post route

---
+++ server/migrations/03_20250409181132_chat_members.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTable("chat_members", (table) => {
+		table.increments("id").primary();
+		table.integer("chat_id");
+		table.foreign("chat_id").references("users.id").onDelete("CASCADE");
+		table.integer("user_id");
+		table.foreign("user_id").references("users.id").onDelete("CASCADE");
+		table.timestamps(true, true);
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema
+		.alterTable("chat_members", (table) => {
+			table.dropForeign("chat_id");
+			table.dropForeign("user_id");
+		})
+		.then(function () {
+			return knex.schema.dropTableIfExists("chat_members");
+		});
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,24 @@
 const path = require("path");
 const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
 require("dotenv").config({ path: "/app_data/.env" });
 const express = require("express");
 const app = express();
 var PORT = process.env.SERVER_PORT;
+var CLIENT_PORT = process.env.CLIENT_PORT;
 const cors = require("cors");
 
 if (!PORT) {
 	dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 	PORT = process.env.SERVER_PORT;
+	CLIENT_PORT = process.env.CLIENT_PORT;
 }
 
 const socket = http.createServer(app);
 const io = new Server(socket, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: `http://localhost:${CLIENT_PORT}`,
 		methods: ["GET", "POST"],
 	},
 });
@@ -34,13 +38,21 @@ io.on("connection", (socket) => {
 	console.log("User connected:", socket.id);
 
 	socket.on("send_message", (data) => {
-		// optionally save to DB here
 		io.emit("receive_message", data);
 	});
 
 	socket.on("disconnect", () => {
 		console.log("User disconnected", socket.id);
 	});
+});
+
+app.use((req, res) => {
+	res.status(404).json({ error: "Not Found" });
+});
+
+app.use((err, req, res) => {
+	console.error("Server error:", err);
+	res.status(500).json({ error: "Internal Server Error" });
 });
 
 const server = socket.listen(PORT, () => {
