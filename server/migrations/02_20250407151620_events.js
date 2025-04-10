ed0201da91a39362a90b83f2771f2d14bf83a029
Adam-Brace <adambrace03@gmail.com>
2025-04-10 10:34:32 -0400

    update db

--- server/migrations/02_20250407151620_events.js
+++ server/migrations/02_20250407151620_events.js
@@ -7,11 +7,14 @@ exports.up = function (knex) {
 		table.increments("id").primary();
 		table.integer("user_id");
 		table.foreign("user_id").references("users.id").onDelete("CASCADE");
+		table.integer("chat_id");
+		table.foreign("chat_id").references("chats.id").onDelete("CASCADE");
 		table.datetime("start_date").notNullable();
 		table.datetime("end_date").notNullable();
 		table.string("title").notNullable();
 		table.string("description");
 		table.string("color");
+		table.string("status").defaultTo("pending");
 		table.timestamps(true, true);
 	});
 };
@@ -24,6 +27,7 @@ exports.down = function (knex) {
 	return knex.schema
 		.alterTable("events", (table) => {
 			table.dropForeign("user_id");
+			table.dropForeign("chat_id");
 		})
 		.then(function () {
 			return knex.schema.dropTableIfExists("events");
