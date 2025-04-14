b0a434c1c4611138c7d8fc36a7351a5303fb916b
Adam-Brace <adambrace03@gmail.com>
2025-04-14 14:45:58 -0400

    merge fix

--- client/src/UserData/UserData.jsx
+++ client/src/UserData/UserData.jsx
@@ -1,14 +1,9 @@
-
 import { useEffect, useState } from "react";
 import Edit from "./Edit";
 import "./UserData.css";
 const API_URL = import.meta.env.VITE_API_URL;
-
-import { useAuth } from '../Context/AuthContext';
-import '../styles/Form.css';
-
-
 import { useAuth } from "../Context/AuthContext";
+import "../styles/Form.css";
 
 export default function UserData() {
 	const [userdata, setUserdata] = useState([]);
@@ -34,38 +29,55 @@ export default function UserData() {
 					: "Loading..."}
 			</h1>
 
-
-
-  return (
-    <>
-    <h1 className="user-name">{userdata.length > 0 ? `${userdata[0].rank} ${userdata[0].last_name}'s Profile` : "Loading..."}</h1>
-
-    <div className="auth-container">
-        <ul className="user-list">
-          {userdata.length == 0 ? (
-            <li className="error-message">Error. No User Data Found</li>
-          ) : (
-          userdata.map(user => (
-            <li key={user.id} className="user-card">
-              <p><strong>First Name:</strong> {user.first_name}</p>
-              <p><strong>Last Name:</strong> {user.last_name}</p>
-              <p><strong>Rank:</strong> {user.rank}</p>
-              <p><strong>Email:</strong> {user.email}</p>
-              <p><strong>Duty Phone #:</strong> {user.phone}</p>
-              <p><strong>Organization:</strong> {user.organization}</p>
-              <p><strong>Crew:</strong> {user.crew}</p>
-              <p><strong>Position:</strong> {user.position}</p>
-              <p><strong>Role:</strong> {user.permissions}</p>
-        <div className='edit-container'>
-        <Edit id={user.id} currentData={{...user}}/>
-        </div>
-              </li>
-          ))
-        )}
-        </ul>
-      </div>
-
-    </>
-  )
+			<div className="auth-container">
+				<ul className="user-list">
+					{userdata.length == 0 ? (
+						<li className="error-message">
+							Error. No User Data Found
+						</li>
+					) : (
+						userdata.map((user) => (
+							<li key={user.id} className="user-card">
+								<p>
+									<strong>First Name:</strong>{" "}
+									{user.first_name}
+								</p>
+								<p>
+									<strong>Last Name:</strong> {user.last_name}
+								</p>
+								<p>
+									<strong>Rank:</strong> {user.rank}
+								</p>
+								<p>
+									<strong>Email:</strong> {user.email}
+								</p>
+								<p>
+									<strong>Duty Phone #:</strong> {user.phone}
+								</p>
+								<p>
+									<strong>Organization:</strong>{" "}
+									{user.organization}
+								</p>
+								<p>
+									<strong>Crew:</strong> {user.crew}
+								</p>
+								<p>
+									<strong>Position:</strong> {user.position}
+								</p>
+								<p>
+									<strong>Role:</strong> {user.permissions}
+								</p>
+								<div className="edit-container">
+									<Edit
+										id={user.id}
+										currentData={{ ...user }}
+									/>
+								</div>
+							</li>
+						))
+					)}
+				</ul>
+			</div>
+		</>
+	);
 }
-
